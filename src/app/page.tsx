7d2a2b1e148c111c1b0de5d244097e23b6e1be53
s-hirano-ist <s-hirano-ist@outlook.com>
2024-06-08 14:02:25 +0900

    feat: 🎸 add table for queue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,97 @@
-import { MainTab } from "@/features/blog/components/main-tab";
+import { SubmitButton } from "@/components/form/submit-button";
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Form } from "@/features/blog/components/form";
+import { QueuedTable } from "@/features/blog/components/queued-table";
+import prisma from "@/server/db";
+import { GitHubLogoIcon, PlusIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
-export default function Home() {
-	return <MainTab />;
+export default async function Home() {
+	const newsDetail = await prisma.newsDetail.findMany({
+		where: {
+			exported: false,
+		},
+		select: {
+			title: true,
+			quote: true,
+			url: true,
+			news: {
+				select: {
+					heading: true,
+				},
+			},
+		},
+	});
+	const data = newsDetail.map((d) => {
+		return {
+			title: d.title,
+			quote: d.quote,
+			url: d.url,
+			category: d.news?.heading ?? "",
+		};
+	});
+
+	return (
+		<Card className="w-full">
+			<CardHeader>
+				<CardTitle>エクスポート待ち</CardTitle>
+				<CardDescription>LocalのGitへ書き込み待ちのデータ一覧</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<QueuedTable data={data} />
+			</CardContent>
+			<CardFooter className="flex justify-between">
+				<Drawer>
+					<div className="grid grid-cols-2 gap-4 w-full">
+						<DrawerTrigger asChild>
+							<Button variant="outline">
+								<PlusIcon />
+							</Button>
+						</DrawerTrigger>
+						<Link href={process.env.GITHUB_LINK ?? ""} target="_blank">
+							<Button variant="outline" className="w-full">
+								<GitHubLogoIcon />
+							</Button>
+						</Link>
+					</div>
+					<DrawerContent>
+						<DrawerHeader>
+							<DrawerTitle>新規登録</DrawerTitle>
+							<DrawerDescription>
+								ブログに登録するデータを入力してください。
+							</DrawerDescription>
+						</DrawerHeader>
+						<Form>
+							<DrawerFooter>
+								<div className="grid grid-cols-2 gap-4">
+									<DrawerClose asChild>
+										<Button variant="outline">キャンセル</Button>
+									</DrawerClose>
+									<SubmitButton label="保存" />
+								</div>
+							</DrawerFooter>
+						</Form>
+					</DrawerContent>
+				</Drawer>
+			</CardFooter>
+		</Card>
+	);
 }
--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -5,7 +5,7 @@ import * as ToastPrimitives from "@radix-ui/react-toast";
 import { type VariantProps, cva } from "class-variance-authority";
 import * as React from "react";
 
-import { cn } from "@/libs/utils";
+import { cn } from "@/lib/utils";
 
 const ToastProvider = ToastPrimitives.Provider;
 
