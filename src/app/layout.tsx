cac2a0a18cb5db704271679a775da78e1a1ff3a7
s-hirano-ist <s-hirano-ist@outlook.com>
2024-06-09 09:57:54 +0900

    fix: 🐛 padding on mobile devices
    ✅ Closes: #53

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 	return (
 		<html lang="ja">
 			<body className={inter.className}>
-				<main className="flex min-h-screen flex-col items-center justify-between">
+				<main className="flex min-h-screen flex-col items-center justify-between p-4">
 					{children}
 					<Toaster />
 				</main>
