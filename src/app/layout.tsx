b9e516ea9a8ec7d9e33475965490ec8ff35f9fc1
s-hirano-ist <s-hirano-ist@outlook.com>
2024-06-18 22:09:16 +0900

    refactor: 💡 metadata and types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,18 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import type { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Dump to GitHub",
+	title: "%s | Dump",
 	description: "Dump blog data to GitHub",
 };
 
 export default function RootLayout({
 	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+}: Readonly<{ children: ReactNode }>) {
 	return (
 		<html lang="ja">
 			<body className={inter.className}>
