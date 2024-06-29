55b0e97e7fd145aaed96c8a3fc0510ff94d343e2
s-hirano-ist <s-hirano-ist@outlook.com>
2024-06-29 16:53:31 +0900

    refactor: 💡 use as const for constants

--- src/features/blog/constants.ts
+++ src/features/blog/constants.ts
@@ -1,14 +1,14 @@
 export const FORM_ERROR_MESSAGES = {
 	REQUIRED: "必須項目です。",
 	TOO_LONG: "文字数が多すぎます。",
-};
+} as const;
 
 export const ERROR_MESSAGES = {
 	UNEXPECTED: "予期せぬエラーが発生しました。",
 	LINE: "Line Notifyでエラーが発生しました。",
 	INVALID_FORMAT: "無効なフォーマットで入力されています。",
-};
+} as const;
 
 export const SUCCESS_MESSAGES = {
 	SUCCESS: "正常に登録できました。",
-};
+} as const;
