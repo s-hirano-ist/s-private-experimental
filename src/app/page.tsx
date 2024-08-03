8831f1da199b509132c6e4bf1d6effb28846a66b
s-hirano-ist <s-hirano-ist.outlook.com>
2024-08-03 10:09:27 +0900

    feat: 🎸 add main page

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Header } from "@/components/nav/header";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Notes | Contents",
+	description: "Private notes",
+};
+
+export default function Page() {
+	return (
+		<>
+			<Header title="" />
+		</>
+	);
+}
