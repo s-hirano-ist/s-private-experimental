1b648f02b2f1efff30b9b47e44da9662ef1ce7d6
s-hirano-ist <s-hirano-ist.outlook.com>
2024-09-23 11:26:50 +0900

    refactor: 💡 throw error instead of returning non url

--- src/lib/sanitize-href.ts
+++ src/lib/sanitize-href.ts
@@ -1,7 +1,10 @@
 export const sanitizeHref = (url: string): string => {
-	if (/^https?:\/\//.exec(url)) {
-		return url;
-	}
-	console.error("Detected url which is not HTTPS.");
-	return "NON URL STRING";
+	if (/^https?:\/\//.exec(url)) return url;
+	throw new Error("Detected url which is not HTTPS");
 };
+
+/* 下記のような対策でもOK
+const urlObj = new URL(url);
+if (urlObj.protocol === "http:" || urlObj.protocol === "https:") {
+}
+*/
