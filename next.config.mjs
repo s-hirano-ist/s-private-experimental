afc5d2af2e5d67b2da539cf07d365cbe07e5e2db
s-hirano-ist <s-hirano-ist.outlook.com>
2024-09-23 12:05:11 +0900

    refactor: 💡 add no nonce on csp

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,36 @@
 await import("./src/env.mjs");
 
+//MEMO: scriptタグを利用する必要が出たときはnonceの利用推奨
+// https://nextjs.org/docs/app/building-your-application/configuring/content-security-policy#reading-the-nonce
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	experimental: { typedRoutes: true },
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value: cspHeader.replace(/\n/g, ""),
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,45 +3,8 @@ import { DEFAULT_SIGN_IN_REDIRECT } from "./constants";
 export const publicRoutes: string[] = [];
 export const authRoutes: string[] = ["/auth"];
 export const apiAuthPrefix = "/api/auth";
-import { NextResponse } from "next/server";
 
 export default auth((request) => {
-	const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-	const cspHeader = `
-	  default-src 'self';
-	  script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-	  style-src 'self' 'nonce-${nonce}';
-	  img-src 'self' blob: data:;
-	  font-src 'self';
-	  object-src 'none';
-	  base-uri 'self';
-	  form-action 'self';
-	  frame-ancestors 'none';
-	  upgrade-insecure-requests;`;
-
-	// Replace newline characters and spaces
-	const contentSecurityPolicyHeaderValue = cspHeader
-		.replace(/\s{2,}/g, " ")
-		.trim();
-
-	const requestHeaders = new Headers(request.headers);
-	requestHeaders.set("x-nonce", nonce);
-
-	requestHeaders.set(
-		"Content-Security-Policy",
-		contentSecurityPolicyHeaderValue,
-	);
-
-	const response = NextResponse.next({
-		request: {
-			headers: requestHeaders,
-		},
-	});
-	response.headers.set(
-		"Content-Security-Policy",
-		contentSecurityPolicyHeaderValue,
-	);
-
 	const { nextUrl } = request;
 	const auth = request.auth;
 	const isLoggedIn = !!auth;
@@ -50,24 +13,20 @@ export default auth((request) => {
 	const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
 	const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-	if (isApiAuthRoute) return response;
+	if (isApiAuthRoute) return;
 
 	if (isAuthRoute) {
 		if (isLoggedIn)
 			return Response.redirect(new URL(DEFAULT_SIGN_IN_REDIRECT, nextUrl));
-		return response;
+		return;
 	}
 
 	if (!isLoggedIn && !isPublicRoute)
 		return Response.redirect(new URL("/auth", nextUrl));
 
-	return response;
+	return;
 });
 
 export const config = {
 	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-	missing: [
-		{ type: "header", key: "next-router-prefetch" },
-		{ type: "header", key: "purpose", value: "prefetch" },
-	],
 };
