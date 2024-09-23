e11c67e4943400ece1308594277b554498617dc3
s-hirano-ist <s-hirano-ist.outlook.com>
2024-09-23 11:32:04 +0900

    refactor: 💡 sanitize href script

--- src/lib/sanitize-href.ts
+++ src/lib/sanitize-href.ts
@@ -1,10 +1,8 @@
 export const sanitizeHref = (url: string): string => {
-	if (/^https?:\/\//.exec(url)) return url;
+	const urlObj = new URL(url);
+	if (urlObj.protocol === "http:" || urlObj.protocol === "https:") return url;
 	throw new Error("Detected url which is not HTTPS");
+	// 下記のような対策でもOK
+	// if (/^https?:\/\//.exec(url)) return url;
+	// throw new Error("Detected url which is not HTTPS");
 };
-
-/* 下記のような対策でもOK
-const urlObj = new URL(url);
-if (urlObj.protocol === "http:" || urlObj.protocol === "https:") {
-}
-*/
