076b822d1964c76abe1abef5c9478bcb27b8156a
s-hirano-ist <s-hirano-ist.outlook.com>
2024-10-05 08:02:44 +0900

    fix: 🐛 sentry build error

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,10 +2,11 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
+import { env } from "@/env.mjs";
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-	dsn: "https://cee646a424e92410d7f08ae439cf1fbf@o4508058782531584.ingest.us.sentry.io/4508058784825344",
+	dsn: env.SENTRY_DSN,
 
 	// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
 	tracesSampleRate: 1,
