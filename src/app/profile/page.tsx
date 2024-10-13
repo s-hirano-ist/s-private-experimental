9e615a0529c500dafe42ff02ae20347100e50ad4
s-hirano-ist <s-hirano-ist.outlook.com>
2024-10-13 18:53:04 +0900

    fix: 🐛 build

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,8 +3,8 @@ import { getSelfScope } from "@/apis/prisma/fetch-user";
 import { Header } from "@/components/nav/header";
 import { PAGE_NAME } from "@/constants";
 import { checkSelfAuth } from "@/features/auth/utils/get-session";
-import { ScopeUpdateSwitch } from "@/features/profile/components/scopeuupdatewswitch";
-import { UpsertProfileForm } from "@/features/profile/components/upserttprofilelform";
+import { ScopeUpdateSwitch } from "@/features/profile/components/scope-update-switch";
+import { UpsertProfileForm } from "@/features/profile/components/upsert-profile-form";
 import type { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
