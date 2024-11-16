fdfb6bc93b70cd2071a113cc33e7ff0e75b3a0ed
s-hirano-ist <s-hirano-ist@outlook.com>
2024-11-16 11:17:08 +0900

    feat: 🎸 add page preview

--- src/app/(dumper)/@image/page.tsx
+++ src/app/(dumper)/@image/page.tsx
@@ -1,27 +1,46 @@
-import { StackSkeleton } from "@/components/stack/stack-skeleton";
+import { StatusCodeView } from "@/components/status-code-view";
 import { Separator } from "@/components/ui/separator";
+import { ERROR_MESSAGES } from "@/constants";
 import { checkPostPermission } from "@/features/auth/utils/role";
 import { AddFormSkeleton } from "@/features/dump/components/add-form-skeleton";
 import { AddImageProvider } from "@/features/dump/components/add-image-provider";
 import { ImageStackProvider } from "@/features/dump/components/image-stack-provider";
+import { ImageStackSkeleton } from "@/features/image/components/image-stack-skeleton";
+import { loggerError } from "@/pino";
 import { Suspense } from "react";
 
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-	const hasPostPermission = await checkPostPermission();
+	try {
+		const hasPostPermission = await checkPostPermission();
 
-	return (
-		<>
-			{hasPostPermission && (
-				<Suspense fallback={<AddFormSkeleton showCategory />}>
-					<AddImageProvider />
+		return (
+			<>
+				{hasPostPermission && (
+					<Suspense fallback={<AddFormSkeleton showCategory />}>
+						<AddImageProvider />
+					</Suspense>
+				)}
+				<Separator className="h-px bg-gradient-to-r from-primary to-primary-grad" />
+				<Suspense fallback={<ImageStackSkeleton />}>
+					<ImageStackProvider />
 				</Suspense>
-			)}
-			<Separator className="h-px bg-gradient-to-r from-primary to-primary-grad" />
-			<Suspense fallback={<StackSkeleton />}>
-				<ImageStackProvider />
-			</Suspense>
-		</>
-	);
+			</>
+		);
+	} catch (error) {
+		loggerError(
+			ERROR_MESSAGES.UNEXPECTED,
+			{
+				caller: "ImageStackProvider",
+				status: 500,
+			},
+			error,
+		);
+		return (
+			<div className="flex flex-col items-center">
+				<StatusCodeView statusCode="500" />
+			</div>
+		);
+	}
 }
--- src/components/stack/image-skeleton.tsx
+++
@@ -1,5 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
-export function ImageSkeleton() {
-	return <Skeleton className="h-32 w-full" />;
-}
